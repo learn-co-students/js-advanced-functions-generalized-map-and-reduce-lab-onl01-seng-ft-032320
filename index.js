42c9296286ced0282f1183f5819f937e94f490b8
BenWix <benwichser@gmail.com>
2020-07-02 18:00:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(arr, func) {
+    let newArr = []
+    arr.forEach(x => newArr.push(func(x)))
+    return newArr
+}
+
+function reduce(arr, func, start=null) {
+    let status
+    if (start !== null) {
+        status = start
+    } else {
+        status = arr.shift()
+    }
+    arr.forEach(x => status = func(x,status))
+    return status
+}
