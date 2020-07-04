4f4c31e40524c6744cc26574cb10dcd6b59bc5b6
antdp425 <antdp425@aol.com>
2020-07-04 14:37:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+function map(array, fn){
+   let result = []
+   array.forEach(el => {
+      result.push(fn(el))
+   });
+   return result
+}
+
+function reduce(src, cb, starting){
+   let r = (!!starting) ? starting : src[0]
+   let i = (!!starting) ? 0 : 1
+ 
+   for (; i < src.length; i++) {
+     r = cb(src[i], r)
+   }
+ 
+   return r;
+ }
+
+// let reduce = 
