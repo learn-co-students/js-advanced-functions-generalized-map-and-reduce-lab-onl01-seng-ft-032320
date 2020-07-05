9c44c062ed72cb3272a2b07eeb576986617b5244
Diane Crame <dcrame@icloud.com>
2020-07-04 20:34:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(arr, block) {
+    let result = []
+    for(let i = 0; i < arr.length; i++) {
+        result.push(block(arr[i]))
+    }
+    return result;
+}
+
+function reduce(arr, block, startingPoint=0){
+    
+    let result;
+    let i;
+
+    if(Boolean(startingPoint)){
+      result = startingPoint
+      i = 0
+    } else {
+      result = arr[0]
+      i = 1
+    }
+  
+    for(i; i < arr.length; i++){
+      result = block(result, arr[i])
+    }
+    return result;
+  }
