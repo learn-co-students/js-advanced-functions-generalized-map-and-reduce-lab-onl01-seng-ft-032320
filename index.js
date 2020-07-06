6954269f6792eaa1ed62feb4826f8f580326a0d8
samiylo <skryshu@gmail.com>
2020-07-06 15:07:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+function map(array, f) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(f(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, fun, starting) {
+    let total = (!!starting) ? starting : array[0]
+    let i = (!!starting) ? 0 : 1
+    for (; i < array.length; i++){
+        total = fun(array[i], total)
+    }
+    return total
+}
