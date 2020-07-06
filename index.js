53991d717af68a30eacac2b796bff52bf1acaedd
jrestrepo922 <tinto922@gmail.com>
2020-07-06 14:58:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your functions here
+
+function map(array, callBackFunction){
+    let newArray = []
+    for (const e of array) {
+        newArray.push(callBackFunction(e))
+    }
+    return newArray
+}
+
+
+
+
+
+function reduce(sourceArray, callBackFunction, startingpoint){
+    let i;
+
+    if (startingpoint === undefined){
+        startingpoint = sourceArray[0]
+        i = 1
+    } else {
+        i = 0 
+    }
+    let total = startingpoint
+    
+    for( i ;  i < sourceArray.length; i++){
+        total = callBackFunction(total, sourceArray[i])
+    }
+
+    return total
+}
+
+
+
+reduce([1, 2, true, "razmatazz"], function(a, memo){ return !!a && !!memo})
