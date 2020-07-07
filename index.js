9ff9a610e27f2c0f6c45e580825ad5ddf3c248da
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-07-06 20:41:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+let negatives = (a) => (a * -1);
+let originals = (a) => (a);
+let doubles = (a) => (a*2);
+let squared = (a) => (a *a);
+
+funcArray = [negatives, originals, doubles, squared]
+randomFunc = funcArray[Math.round(Math.random() * funcArray.length)];
+
+function map(arr, randomFunc) {
+  let new_arr = []
+  for(let i in arr) {
+    new_arr.push(randomFunc(arr[i]));
+  };
+  return new_arr;
+};
+
+
+function reduce(arr, randomFunc2, start) {
+  let returnVal = (start) ? start : arr[0];
+  for(let i = (start) ? 0 : 1; i < arr.length; i++) {
+    returnVal = randomFunc2(arr[i], returnVal)
+  };
+  return returnVal;
+};
