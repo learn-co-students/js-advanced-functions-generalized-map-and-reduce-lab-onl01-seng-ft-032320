4ee1194056dc5e0f12037b9ce7b43cc92a2a5b75
ferrisbueller66 <github email address>
2020-07-08 15:40:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+
+ function map(src, functionOfChoice) {
+   let array = []
+   for (let i = 0; i < src.length; i++ ) {
+     array.push(functionOfChoice(src[i]))
+   }
+   return array
+ }
+ 
+ function reduce(src, cb, starting){
+   let r = (!!starting) ? starting : src[0]
+   let i = (!!starting) ? 0 : 1
+   for (; i < src.length; i++) {
+     r = cb(src[i], r)
+   }
+   return r;
+ }
+
+ //this code got down to only 1 test failing;
+
+//  function reduce(sourceArray, reducer, init=0){
+//    let accum = init;
+//    for (let i = 0; i < sourceArray.length; i++){
+//        accum = reducer(sourceArray[i], accum)
+//    }
+//    return accum
+//  }
+  
