decc63fb33931d8417e36f43ab6a693246c7f76e
Ryan-McFadden <ryanmcfadden123@gmail.com>
2020-07-08 00:45:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(src, call) {
+
+    let r = []
+  
+    for (let i = 0; i < src.length; i++) {
+      let theElement = src[i]
+      r.push(call(theElement))
+    }
+  
+    return r;
+}
+  
+function reduce(src, call, starting){
+
+    let r = (!!starting) ? starting : src[0]
+
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = call(src[i], r)
+    }
+  
+    return r;
+}
