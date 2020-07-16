910c9fac30387ddfbe557ae3e360b635d4d44246
nisticism <nistacular@gmail.com>
2020-07-16 00:00:37 -0700

    Done.

--- index.js
+++ index.js
@@ -4,31 +4,13 @@ function map(array, f) {
     return array.map(element => f(element));
 }
 
-function reduce(array, f, startingPoint) {
-    if (!startingPoint) {
-        startingPoint = 0;
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
     }
-    let count = 0;
-    let hasNaNs = false;
-    for (let j = 0; j < array.length; j ++) {
-        if (isNaN(array[j])) {
-            hasNaNs = true;
-        }
-    }
-    for (let i = 0; i < array.length; i ++) {
-        if (hasNaNs) {
-            if (array[i] == false) {
-                return false;
-            } 
-            
-            else if (array[i] = true) {
-                count += 1;
-                if (count >= array.length) {
-                    return true;
-                }
-            }
-        }
-
-    }
-    return array.reduce(f, startingPoint);
-}
+  
+    return r;
+  }
