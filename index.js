d781d9ebac74446d1110bb415512706956cf261e
Beth Ruff <beth.ruff2@gmail.com>
2020-07-20 18:20:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(array, callback) {
+    let newArray = []
+    array.forEach(n => {
+        newArray.push(callback(n))
+    });
+    return newArray 
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+    return r;
+  }
